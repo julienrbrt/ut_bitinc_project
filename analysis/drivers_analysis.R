f9759bbed810b604649256c799eaccdd224d13e5
Julien <julien@rbrt.fr>
2020-03-03 13:04:59 +0100

    setup R

--- analysis/drivers_analysis.R
+++ analysis/drivers_analysis.R
@@ -1,3 +1,6 @@
 # Drivers Driving Style Analysis in R
 
-print("It's works!")
+# Load required libraries
+rLib <- "./analysis/Rlib"
+if (!require(tidyverse)) install.packages("tidyverse", dependencies = TRUE, lib = rLib)
+library(tidyverse)
