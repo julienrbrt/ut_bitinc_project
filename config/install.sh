9830ca2d942cdd6dc6bdb387f90815b8872dee83
Julien <julien@rbrt.fr>
2020-03-04 15:07:27 +0100

    improve installation script

--- config/install.sh
+++ config/install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# General dependencies
+apt-get -y install build-essential curl
+
 # Go dependencies
 echo "Installing Go depencencies..."
-apt install snapd
+apt-get -y install snapd
 snap install Go
 
 # R dependencies
@@ -11,8 +14,8 @@ curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 ### https://docs.microsoft.com/en-us/sql/connect/odbc/linux-mac/installing-the-microsoft-odbc-driver-for-sql-server?view=sql-server-ver15
 curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 apt-get update
-ACCEPT_EULA=Y apt-get install msodbcsql17 build-essential r-base r-base-dev unixodbc unixodbc-dev libcurl4-openssl-dev
+ACCEPT_EULA=Y apt-get -y install msodbcsql17 r-base r-base-dev unixodbc unixodbc-dev libcurl4-openssl-dev
 ln -sr odbcinst.ini ~/.odbcinst.ini
 
 # Redis dependencies
-apt install redis
+apt-get -y install redis
