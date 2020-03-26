8bf5b95361e6b40b5c13b5da55150d714f2c148c
Julien <julien@rbrt.fr>
2020-03-26 17:12:01 +0100

    Simplify installation script

--- config/README.md
+++ config/README.md
@@ -2,5 +2,5 @@
 
 This folder contains files used to setup the project. Thoses files might require modification depending on which system `tx2db` is deployed. 
 
-* [install.sh](install.sh) permits to install all the requirements for running properly the program
+* [install.sh](install.sh) permits to install all the requirements for running properly the program. Do no run the program from the `config` folder without using root (`sudo` necessary).
 * [odbcinst.ini](odbcinst.ini) contains the configuration of the SQL Server library. Might require to be updated in case of library update
--- config/install.sh
+++ config/install.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-# General dependencies
-sudo apt-get -y install build-essential curl
-
-# Go dependencies
-echo "Installing Go depencencies..."
-sudo apt-get -y install snapd
-sudo snap install go
-
-# R dependencies
-echo "Installing R dependencies..."
+# Dependencies
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 ### https://docs.microsoft.com/en-us/sql/connect/odbc/linux-mac/installing-the-microsoft-odbc-driver-for-sql-server?view=sql-server-ver15
 sudo su -c "curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list > /etc/apt/sources.list.d/mssql-release.list"
 sudo apt-get update
-ACCEPT_EULA=Y sudo apt-get -y install msodbcsql17 r-base r-base-dev unixodbc unixodbc-dev libcurl4-openssl-dev libssl-dev libudunits2-dev libfontconfig1-dev libcairo2-dev libgdal-dev pandoc
+ACCEPT_EULA=Y sudo apt-get -y install build-essential curl snapd msodbcsql17 r-base r-base-dev unixodbc unixodbc-dev libcurl4-openssl-dev libssl-dev libudunits2-dev libfontconfig1-dev libcairo2-dev libgdal-dev pandoc
+sudo snap install go
 ln -sr odbcinst.ini ~/.odbcinst.ini
 sudo chmod 777 -R /usr/local/lib/R/site-library
